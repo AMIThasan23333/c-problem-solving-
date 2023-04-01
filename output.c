c0df208502c31647c133c577b1559ef063b29e15
My Name <cpgsom819@gmail.com>
2023-04-02 03:34:58 +0800

    taking input

---
+++ output.c
@@ -0,0 +1,14 @@
+
+  #include<stdio.h>
+  
+  int main(){
+
+    int  rahim ;
+
+    scanf("%d", &rahim);
+
+    printf("%d", rahim);
+
+
+
+  }
---
+++ output2.c
@@ -0,0 +1,22 @@
+
+  #include<stdio.h>
+  
+  int main(){
+
+    int  rahim, karim;
+
+    char c;
+
+    float f;
+
+
+    scanf("%d %d",  &rahim, &karim);
+
+    printf("%d %d", rahim,karim );
+
+
+   return 0;
+
+   
+
+  }
