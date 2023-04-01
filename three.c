35bc23a862680d76cf7b71791829b74af49bc397
My Name <cpgsom819@gmail.com>
2023-04-02 03:45:04 +0800

    taking input

---
+++ three.c
@@ -0,0 +1,23 @@
+
+
+  #include<stdio.h>
+  
+  int main(){
+
+    int  rahim,karim;
+
+    char c;
+    float f;
+
+
+    scanf("%d %f %c", &rahim, &f,&c);
+
+    printf("%d  %0.4f %c" , rahim,f,c);
+
+    return 0;
+
+
+
+
+
+  }
