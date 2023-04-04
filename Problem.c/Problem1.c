bee04017623024481d9c07ce270ed39e2f435e47
My Name <cpgsom819@gmail.com>
2023-04-05 06:34:46 +0800

    done

---
+++ Problem.c/Problem1.c
@@ -0,0 +1,10 @@
+
+/* what will be output of following code  */
+#include <stdio.h>
+int main()
+{
+    int k;
+    for (k = -3; k < -5; k++)
+        printf("Hello ");
+    return 0;
+}
---
+++ Problem.c/Problem2.c
@@ -0,0 +1,18 @@
+
+/* what will be output of following code  */
+
+
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    while (i < 3) {
+
+       
+        i++;
+
+       
+    }
+   printf("%d",i);
+   return 0;
+}
