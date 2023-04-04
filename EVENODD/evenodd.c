8a5fbd49599582b9b3d57eefe71260eed3218fe2
My Name <cpgsom819@gmail.com>
2023-04-05 05:31:08 +0800

    evenodd

---
+++ EVENODD/evenodd.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+
+int main(){
+
+
+      int n, i;
+
+      scanf("%d", &n);
+
+      for( i =0; i<=n; i++){
+
+       if(i%2==0){
+  
+          printf("%d-even\n", i);
+
+       }
+
+       else{
+         
+           printf("%d-odd\n", i);
+
+       }
+           
+      }
+
+
+    return 0;
+}
