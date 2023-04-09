6cdfc05f14087c99026a93e5c1dbf5fc34f22e1b
My Name <cpgsom819@gmail.com>
2023-04-09 22:41:15 +0800

    finished

---
+++ Problem.c/exam 01/5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, shoes ;
+    scanf("%d", &n);
+ 
+       if( n>=1000 ){
+
+
+        printf('i will buy punjabi');
+
+        shoes = n- 1000;
+
+
+        if( shoes >=500){
+
+            printf(' i will buy shoes ');
+
+
+        }
+
+
+       }else{
+        printf('nothing');
+        
+       }
+
+    return 0;
+}
