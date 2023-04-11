fef23630059959c17531cb6d225dea1be5a86668
My Name <cpgsom819@gmail.com>
2023-04-11 11:59:55 +0800

    checkdigitalphanet

--- module05/checkdigitalphabet.c
+++ module05/checkdigitalphabet.c
@@ -3,8 +3,23 @@
   
   int main(){
 
-    
-     return 0;
+    char x;
 
-   
+    scanf("%c", &x);
+
+    if(x >= '0' && x<='9'){
+
+        printf("IS DIGIT");
+    }else{
+
+        printf("Alphabet\n");
+
+        if (x>='a' && x<='z'){
+            printf("in  small case");
+        }else
+        {
+           printf("Is Capital");
+        }   
+    }
+     return 0;
   }
