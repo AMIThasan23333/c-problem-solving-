78d379fa3ee11d7d5f32453e6ce079aa414f464c
My Name <cpgsom819@gmail.com>
2023-04-16 06:02:02 +0800

    reverse an array

--- Module09/Remove.c
+++ Module09/Remove.c
@@ -1,36 +1,26 @@
 
-  #include<stdio.h>
-  
-  int main(){
 
+  #include<stdio.h>  
+  int main(){
   int n;
+
   scanf("%d", &n);
   int arr[n];
-
   for(int i=0; i<n; i++){
     scanf("%d", &arr[i]);
   }
 
    int pos;
    scanf("%d", &pos);
-
-  for(int i=pos; i<n-1; i++){
-    
+   for(int i=pos; i<n-1; i++){ 
     arr[i] = arr[i+1];
 
   }
 
 
     for(int i=0; i<n; i++){
-    
-   printf("%d", arr[i]);
-
+    printf("%d", arr[i]);
   }
-
-
-
-   
-    
      return 0;
 
    
