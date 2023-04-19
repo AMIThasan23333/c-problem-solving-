aa065b3cecf5984ec55c4a55a0a76d29a5a8ee74
My Name <cpgsom819@gmail.com>
2023-04-20 06:18:41 +0800

    multiplication table done

---
+++ concept02p01/multiplicationtable.c
@@ -0,0 +1,20 @@
+
+  #include<stdio.h>
+  int main(){
+
+    int i,j,n;
+
+    for( j=1; j<=10; j++){
+
+      for(i =1; i<=10; i++){
+
+          printf("%d x %d = %d\n",j,i,i*j);
+         
+      }
+     
+
+    }
+      printf("......");
+
+  }
+
