6d8e5d3340c8ed7cba27d2bda90fb555168cbc96
My Name <cpgsom819@gmail.com>
2023-04-23 23:09:44 +0800

    sizeof int

---
+++ Module10/sizeofint.c
@@ -0,0 +1,31 @@
+
+ 
+ /* number 1  */
+ 
+//   #include<stdio.h>
+  
+//   int main(){
+
+//       int a[5];
+
+//       printf("%d", sizeof(a));
+
+//      return 0;
+
+   
+//   }
+
+
+  #include<stdio.h>
+  
+  int main(){
+
+    int a[5];
+
+    printf("%d", sizeof(a)/sizeof(int));
+
+
+     return 0;
+
+   
+  }
