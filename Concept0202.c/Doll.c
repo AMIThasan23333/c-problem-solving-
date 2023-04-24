3188a9a443fc252344f68cbd0dc6bbb11608e6aa
My Name <cpgsom819@gmail.com>
2023-04-24 13:35:28 +0800

    atoz

--- Concept0202.c/Doll.c
+++ Concept0202.c/Doll.c
@@ -38,6 +38,7 @@
 
 
       }
+      printf("%lld", res);
 
     }
 
---
+++ Concept0202.c/ztoa.c
@@ -0,0 +1,16 @@
+
+  #include<stdio.h>
+  
+  int main(){
+
+    char ch = 'z';
+
+    while (ch >= 'a'){
+        printf("%c\n", ch);
+        ch--;
+        
+    }
+     return 0;
+
+   
+  }
