903279dd6bf6594e62cbe0e52abe3d2121af3376
My Name <cpgsom819@gmail.com>
2023-04-30 22:10:53 +0800

    sum of two

--- Module13-1/diamond.c
+++ Module13-1/diamond.c
@@ -1,27 +1,18 @@
-
   #include<stdio.h>
-  
   int main(){
-
-
+   
     int n,s,k;
     scanf("%d", &n);
-
-    s = n-1;
+    s=n-1;
     k=1;
-
     for(int i=1; i<= (2*n) -1; i++){
-
-
         for(int j=1; j<=s; j++){
             printf(" ");
         }
-
            for(int j=1; j<=k; j++){
             printf("*");
         }
         // line sesh
-
      if(i <=n-1){
 
         s--;
