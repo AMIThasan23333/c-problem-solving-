5f2e172a51b2b6c9a9770f9700258a586be12d19
My Name <cpgsom819@gmail.com>
2023-05-15 05:29:22 +0800

    zero matrix

---
+++ Module18/zeromatrix.c
@@ -0,0 +1,46 @@
+
+  #include<stdio.h>
+  
+  int main(){
+
+    int row,col;
+    scanf("%d %d", &row,&col);
+    int a[row][col];
+
+    int ele = row * col;
+    for (int i = 0; i < row; i++)
+    {
+           for (int j = 0; j < col; j++){
+
+             scanf("%d", &a[i][j]);
+
+           }
+    }
+
+    int cnt = 0;
+
+   for (int i = 0; i < row; i++)
+    {
+           for (int j = 0; j < col; j++){
+
+           if (a[i][j] == 0)
+           {
+             cnt++;
+           }
+           
+
+           }
+    }
+
+
+   printf("%d\n",cnt);
+
+   if (ele == cnt)
+   {
+    printf('Zero Matrix');
+   }
+   
+   return 0;
+
+   
+  }
