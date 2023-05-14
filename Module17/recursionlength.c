98de3cbb57c1c06c19bd33a3806df21901611cd0
My Name <cpgsom819@gmail.com>
2023-05-15 03:05:10 +0800

    2 d array

--- Module17/recursionlength.c
+++ Module17/recursionlength.c
@@ -3,24 +3,18 @@
   
 
    int fun ( char a[] , int i ){
-
     if(a[i] == '\0') return 0;
-
-
-     int l = fun(a, i+1);
-     return l+1;
+    int l = fun(a, i+1);
+    return l+1;
 
    }
 
   int main(){
 
-   char a[6] = "Jessica";
-
+   char a[6] = "ica";
    int length = fun(a,0);
-
    printf("%d\n", length);
-    
-     return 0;
+   return 0;
 
    
   }
