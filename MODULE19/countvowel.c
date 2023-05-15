f4ec300dcbb2d611426200c838c108b4b77fa546
My Name <cpgsom819@gmail.com>
2023-05-16 07:07:26 +0800

    factorial

--- MODULE19/countvowel.c
+++ MODULE19/countvowel.c
@@ -2,17 +2,13 @@
   #include<stdio.h>
   
   int fun(char s[], int i ){
-
     if(s[i]== '\0'){
         return 0;
     }
-
     int ans = fun(s, i+1);
-
     if(s[i] >= 'A'  && s[i] <='Z'){
         s[i] = s[i] + 32;
     }
-
     if (s[i]== 'a' || s[i] == 'e' || s[i] == 'o' ||  s[i]  == 'u' ||  s[i]== 'i')
     {
        return ans +1;
